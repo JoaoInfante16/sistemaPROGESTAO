dc6ee7f4832441fcafafee2cdf407bccc9862306
JoaoInfante <joao.infante16@gmail.com>
2026-03-21 13:37:39 -0300

    Painel Admin FIX

--- admin-panel/next.config.ts
+++ admin-panel/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.modules = [
+      path.resolve(__dirname, "node_modules"),
+      "node_modules",
+    ];
+    return config;
+  },
+  turbopack: {},
 };
 
 export default nextConfig;
